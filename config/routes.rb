82a982d85c7a62c3903511e4508e24e188271614
zakinao <zakinaororo@gmail.com>
2021-01-19 20:30:10 +0900

    add post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show]
+  resources :photos
+  resources :posts
 end
